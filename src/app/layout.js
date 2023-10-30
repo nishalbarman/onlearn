07f3ab40a788519c853611be97951604a4da437a
Nishal Barman <helptechnoapps@gmail.com>
2023-10-31 01:27:33 +0530

    Navbar created and integrated with application

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
 import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className} pl-[2px] pr-[2px]`}>
+        <Navbar />
         <Toaster />
         {children}
         <Footer />
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,63 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+function Navbar() {
+  const router = useRouter();
+  return (
+    <div className="shadow-[rgba(17,17,26,0.1)_0px_1px_0px] flex items-center  pt-5 pb-5 w-[100%] justify-between pl-3 pr-3 lg:pl-[15%] lg:pr-[15%]">
+      {/* menu icon */}
+      <div className="flex items-center justify-center xl:hidden w-fit h-fit hover:cursor-pointer">
+        <Image src="/icons/menu.png" width={32} height={32} />
+      </div>
+
+      {/* website logo */}
+      <div className="flex gap-1 items-center w-fit h-[100%]">
+        <Image
+          className="align-center w-[50px]"
+          src="/icons/graduation-hat.png"
+          width={500}
+          height={500}
+        />
+        <h2 className="font-bold text-3xl">
+          On<span className="text-[rgb(254,134,28)]">Learn</span>
+        </h2>
+      </div>
+
+      {/* desktop links */}
+      <div className="hidden xl:flex items-center gap-5 w-fit">
+        <Link className="font-bold text-lg" href={"/"}>
+          Home
+        </Link>
+        <Link className="font-bold text-lg" href={"/about"}>
+          About
+        </Link>
+        <Link className="font-bold text-lg" href={"/courses"}>
+          Courses
+        </Link>
+        <Link className="font-bold text-lg" href={"/contact"}>
+          Contact Us
+        </Link>
+      </div>
+
+      {/* signup and login button */}
+      <div className="hidden xl:flex gap-6 items-center h-fill w-fit">
+        <Link className="font-normal text-lg underline" href={"/signup"}>
+          SignUp
+        </Link>
+        <button
+          className="p-2 w-[150px] rounded-lg text-white text-lg font-semibold bg-[rgb(254,134,28)] hover:scale-95 duration-200"
+          onClick={() => {
+            router.push("/login");
+          }}>
+          Login
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
