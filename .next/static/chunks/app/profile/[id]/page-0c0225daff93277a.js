07af553693fe79b9a78a23aaf90f555a6bdb86bd
Nishal Barman <helptechnoapps@gmail.com>
2023-10-31 16:46:39 +0530

    Navbar added

--- .next/static/chunks/app/profile/[id]/page-0c0225daff93277a.js
+++
@@ -1,9 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[495],{2709:function(e,r,t){Promise.resolve().then(t.bind(t,5582))},5582:function(e,r,t){"use strict";t.r(r);var n=t(7437);function ProfileId(e){let{params:r}=e;return(0,n.jsxs)("div",{className:"flex flex-col items-center justify-center min-h-screen py-2",children:[(0,n.jsx)("h1",{className:"text-black text-4xl text-center",children:"Profile"}),(0,n.jsxs)("span",{className:"m-5 p-2 rounded bg-white",children:["Your id is : ",null==r?void 0:r.id]})]})}t(2265),r.default=ProfileId},622:function(e,r,t){"use strict";/**
- * @license React
- * react-jsx-runtime.production.min.js
- *
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var n=t(2265),s=Symbol.for("react.element"),o=Symbol.for("react.fragment"),i=Object.prototype.hasOwnProperty,l=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,c={key:!0,ref:!0,__self:!0,__source:!0};function q(e,r,t){var n,o={},f=null,u=null;for(n in void 0!==t&&(f=""+t),void 0!==r.key&&(f=""+r.key),void 0!==r.ref&&(u=r.ref),r)i.call(r,n)&&!c.hasOwnProperty(n)&&(o[n]=r[n]);if(e&&e.defaultProps)for(n in r=e.defaultProps)void 0===o[n]&&(o[n]=r[n]);return{$$typeof:s,type:e,key:f,ref:u,props:o,_owner:l.current}}r.Fragment=o,r.jsx=q,r.jsxs=q},7437:function(e,r,t){"use strict";e.exports=t(622)}},function(e){e.O(0,[971,472,744],function(){return e(e.s=2709)}),_N_E=e.O()}]);
